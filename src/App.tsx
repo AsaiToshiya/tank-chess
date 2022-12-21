9bb24a73e7ec0218e70e4b357016c8fe4ff6eca9
Asai Toshiya <to.asai.60@gmail.com>
2022-12-21 12:00:45 +0900

    障害物をレンダリングする
    Closes #3

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,31 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
+type Obstacle = {
+  [key: string]: never;
+};
+
+const isObstacle = (space: Obstacle | null) =>
+  space !== null && Object.keys(space).length === 0;
+
 function App() {
   // prettier-ignore
   const [spaces, setSpaces] = useState([
     [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
+    [null, {},   null, null, {},   {},   null, null, null, null, null, null, null, null, null, null],
+    [null, {},   null, null, {},   {},   null, null, null, null, null, {},   {},   null, null, null],
+    [null, {},   null, null, {},   null, null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, {},   {},   null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, {},   {},   {},   {},   {},   null, null, {},   {},   null],
+    [null, null, {},   {},   null, null, null, null, null, null, null, null, null, null, {},   null],
+    [null, null, {},   {},   {},   null, null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null, null, {},   {},   {},   null, null],
+    [null, {},   null, null, null, null, null, null, null, null, null, null, {},   {},   null, null],
+    [null, {},   {},   null, null, {},   {},   {},   {},   {},   null, null, null, null, null, null],
+    [null, null, null, null, null, null, {},   {},   null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null, null, {},   null, null, {},   null],
+    [null, null, null, {},   {},   null, null, null, null, null, {},   {},   null, null, {},   null],
+    [null, null, null, null, null, null, null, null, null, null, {},   {},   null, null, {},   null],
     [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
   ])
 
@@ -28,7 +35,7 @@ function App() {
       {spaces.map((columns, i) => (
         <div className="board-row">
           {columns.map((space, i) => (
-            <button className="space"></button>
+            <button className="space">{isObstacle(space) && "X"}</button>
           ))}
         </div>
       ))}
